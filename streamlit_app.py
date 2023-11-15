d6cba19a8e587f6ce4fcea74b66d977eee8be100
Ajguio <150486535+Ajguio@users.noreply.github.com>
2023-11-15 16:56:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+# Título principal
 st.title("My Parents' New Healthy Diner")
+
+# Títulos adicionales con propiedades
+st.header("Breakfast Menu")
+
+st.text("Omega 3 & Blueberry Oatmeal")
+st.text("Kale, Spinach & Rocket Smoothie")
+st.text("Hard-Boiled Free-Range Egg")
