102fa7f1cd1388bdf8125978e271ae0ae660b636
Ajguio <150486535+Ajguio@users.noreply.github.com>
2023-11-16 19:13:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,6 @@ try:
     else:
         back_from_function = get_fruityvice_data (fruit_choice)        
         st.dataframe(back_from_function)
-
 except URLError as e:
     st.error("Error occurred. Please check your internet connection.")
 
