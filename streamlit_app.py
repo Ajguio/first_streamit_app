f5ede7fda6fd6a989cfc1fc36e20126218d2e2da
Ajguio <150486535+Ajguio@users.noreply.github.com>
2023-11-16 19:15:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,10 +55,11 @@ if st.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     st.dataframe(my_data_rows)
     
+st.stop()
 
 add_my_fruit = st.text_input('What fruit would you like add?','Kiwi')
 st.write('Thanks for adding ', add_my_fruit)
 
 my_cur.execute ("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
 
-st.stop()
+
