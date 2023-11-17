91f003c56636413238b82e07f973200f30916aef
Ajguio <150486535+Ajguio@users.noreply.github.com>
2023-11-16 19:38:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
 except URLError as e:
     st.error("Error occurred. Please check your internet connection.")
 
-st.header("The fruit load list contains:")
+st.header("View Our Fruit List - Add Your Favorites!")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -49,7 +49,7 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 #Add a button to load the fruit
-if st.button('Get Fruit Load List'):
+if st.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
